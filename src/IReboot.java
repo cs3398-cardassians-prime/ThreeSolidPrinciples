64c7a02f4539b877ea332a1667be85941ebbe78d
bsc52 <46979919+bsc52@users.noreply.github.com>
2019-02-17 17:37:35 -0600

    Add files via upload

---
+++ src/IReboot.java
@@ -0,0 +1,12 @@
+package threesolid;
+
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.io.*;
+
+
+interface IReboot {
+    public void reboot();
+}
+
+
