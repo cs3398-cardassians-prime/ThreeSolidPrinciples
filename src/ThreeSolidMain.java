7547a487b0475159c7b4dae2a076b302c39e5152
Rlk81 <rlk81@txstate.edu>
2019-02-18 17:40:15 -0600

    ThreeMainSolid
    Modifed the main program to create these classes and call each method in the classes.

--- src/ThreeSolidMain.java
+++ src/ThreeSolidMain.java
@@ -9,17 +9,19 @@ import java.io.*;
 /*
 1. Open/Closed:Does not apply.
 
-2. Single Responsibility: The main is only responsible for creating the Manager and any exception handling.
-
-
+2. Single Responsibility: The main is only responsible for creating of the classes and calling the methods in the class 
 3. Interface Segregation: Does not apply.
 */
 
 public class ThreeSolidMain
 {   
-
-   public static Manager tsManager = new Manager();
-
+   public static ProductManager tsProductManager = new ProductManager();
+   public static ProjectManager tsProjectManager = new ProjectManager();
+   public static Worker tsWorker= new Worker();
+   public static SuperWorker tsSuperWorker= new SuperWorker();
+   public static TempWorker tsTempWorker = new TempWorker();
+   public static Robot tsRobot = new Robot();
+   
    // The entry main() method
    public static void main(String[] args) 
    {
@@ -27,8 +29,23 @@ public class ThreeSolidMain
       try 
       {
          System.out.format("Starting ... \n"); 
-         tsManager.setWorker(new SuperWorker());
-         tsManager.manage();
+         
+         tsProductManager.schedulework();
+         tsProjectManager.defineproduct();
+        
+         tsWorker.work();
+         
+         tsTempWorker.work();
+         tsTempWorker.eat();
+         tsTempWorker.sick();
+         
+         tsSuperWorker.work();
+         tsSuperWorker.sick();
+         tsSuperWorker.eat();
+                  
+         tsRobot.work();
+         tsRobot.reboot();
+         
       } 
       catch (Exception main_except)
       {
