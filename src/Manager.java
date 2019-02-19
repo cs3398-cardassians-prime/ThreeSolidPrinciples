9d481e649b0bad716a4488afa6c19e2270739b2b
JBringmann <johnathanbringmann@gmail.com>
2019-02-18 20:33:21 -0600

    Updated Manager class
    replaced  IWorkable with BaseWorker

--- src/Manager.java
+++ src/Manager.java
@@ -5,26 +5,18 @@ import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
 
-/*
- * Three principles:
- * 1. Open/Closed: The manger class doesn't 100% display the Open/closed principle due to the fact that this class is never directly 
- * messed with, when trying to change the the worker class.  
- * 2. Single Responsibility: while this class does have 2 separate responsibilities in the fact that it creates workers and also manages,
- * them,  but these both could be viewed as the responsibilites of a manager,
- * 3. Interface Segregation: This principle is not explicitly used in this class.
- */
 
 public class Manager {
-		IWorkable worker;
+		BaseWorker worker;
 
-		public void setWorker(IWorkable w) {   /// changed from Workable to IWorkable
+		public void setWorker(BaseWorker w) {  
 			worker = w;
         
 		}
 
 		public void manage() {
 			worker.work();
-         //worker.eat();
+        
          
 		}
 	
