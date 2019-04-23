918502119c0add57e0cb5f6667ea91e140d84a5d
bsc52 <46979919+bsc52@users.noreply.github.com>
2019-04-23 17:42:45 -0500

    Added suggested starting code from assignment

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,1 +1,14 @@
- 
+
+package testthreesolid;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+public class TestWorker {
+ private Worker w = new Worker();
+ @Test
+ @DisplayName("Work Method Test")
+ public void testWorkMethod()
+ {
+ assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
+ }
+}
