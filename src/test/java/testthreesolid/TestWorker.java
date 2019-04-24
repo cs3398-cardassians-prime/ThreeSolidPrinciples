055faee46d39f342e3015e1d1322b83ca85e0367
FredHanson <47045984+FredHanson@users.noreply.github.com>
2019-04-24 17:27:04 -0500

    still not working 4/24

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,1 +1,29 @@
+ package threesolid; 
  
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+ import static org.junit.Assert.*;
+ import org.junit.Before;
+ import org.junit.Test;
+
+import javafx.concurrent.Worker; 
+ 
+ 
+public class TestWorker {
+	private  Worker<V> w = new Worker();
+	@Test
+	@DisplayName("Work Method Test")
+	public void testWorkMethod() {
+		assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+	} 
+   
+   @Test
+   @DisplayName("FH Pass Test")
+   public void newtestFHWorkerPass() {
+      assertEquals(w.work(),"Working...from BaseWorker\n");
+   }
+   
+   
+   
+}
