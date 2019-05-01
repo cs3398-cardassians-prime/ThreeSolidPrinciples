a7aabb1811082ce5a62c4072f60aa4f0c9597fcc
JBringmann <johnathanbringmann@gmail.com>
2019-04-30 20:11:12 -0500

    Update TestWorker.java
    added my WorkerPass

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,1 +1,31 @@
- 
+package threesolid;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestWorker {
+	private  Worker w = new Worker();
+
+	@Test
+	@DisplayName("Work Method Test")
+	public void testWorkMethod() {
+		assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
+	}
+   
+	@Test
+	@DisplayName("Work Method Test")
+	public void newtestJBWorkerPass() {
+      w = new Manager();
+      Worker robot = new Robot();
+      w.setWorker(robot);
+      assertEquals(w.manager(), "Robot rebooting... \n");
+		assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
+	}
+   
+	// @Test
+	// @DisplayName("Work Method Test")
+	// public void newtest<your_initials>WorkerFail() {
+		// assertEquals(w.work(),"I'm working already!", "Mismatch between test text and method text");
+	// }
+} 
